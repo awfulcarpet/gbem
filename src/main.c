b3495c01df6d5165d3b6f73c95c884fdf24d3c3a
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-07-31 19:41:15 +0000

    add sample sm83 program

--- src/main.c
+++ src/main.c
@@ -11,16 +11,31 @@ main(int argc, char **argv) {
 
 	struct CPU *cpu = init_cpu();
 
-	/*for (int i = 0; i <= 0xFF; i++) {*/
-	/*	cpu->memory[i] = i;*/
-	/*}*/
+	/* load CAFE into DE and BABE into BC and swap them */
+	cpu->memory[0] = 0x11;
+	cpu->memory[1] = 0xFE;
+	cpu->memory[2] = 0xCA;
 
-	cpu->memory[0] = 0x2b;
+	cpu->memory[3] = 0x01;
+	cpu->memory[4] = 0xBE;
+	cpu->memory[5] = 0xBA;
 
-	for (int i = 0; i <= 0xFF; i++) {
-		print_cpu_state(cpu);
+	cpu->memory[6] = 0x78;
+	cpu->memory[7] = 0x42;
+	cpu->memory[8] = 0x57;
+	cpu->memory[9] = 0x79;
+	cpu->memory[10] = 0x4b;
+	cpu->memory[11] = 0x5f;
+
+	cpu->memory[12] = 0x10; /* STOP */
+
+	print_cpu_state(cpu);
+	while (1) {
 		cpu->mcycles += execute(cpu);
 		print_cpu_state(cpu);
+
+		if (cpu->stop == 1)
+			break;
 	}
 	return 0;
 }
