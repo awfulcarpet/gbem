1c65f1f70a81d751e5127ee46e49cbc85c31a250
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-08-06 20:18:05 +0000

    change main.c to (attempt to) run tetris

--- src/main.c
+++ src/main.c
@@ -22,45 +22,10 @@ main(int argc, char **argv) {
 
 	if (gb == NULL) return 1;
 
-	/* load CAFE into DE and BABE into BC and swap them */
-	// cpu->memory[0] = 0x11;
-	// cpu->memory[1] = 0xFE;
-	// cpu->memory[2] = 0xCA;
-	//
-	// cpu->memory[3] = 0x01;
-	// cpu->memory[4] = 0xBE;
-	// cpu->memory[5] = 0xBA;
-	//
-	// cpu->memory[6] = 0x78;
-	// cpu->memory[7] = 0x42;
-	// cpu->memory[8] = 0x57;
-	// cpu->memory[9] = 0x79;
-	// cpu->memory[10] = 0x4b;
-	// cpu->memory[11] = 0x5f;
-	// cpu->memory[12] = 0xe0;
-	// cpu->memory[13] = 0x02;
-	// cpu->memory[0xff01] = 'c';
-	//
-	// cpu->memory[14] = 0xcd; /* CALL a16 */
-	// cpu->memory[15] = 0x17;
-	// cpu->memory[16] = 0x00;
-	//
-	// cpu->memory[0x0017] = 0xcd; /* CALL a16 */
-	// cpu->memory[0x0018] = 0x20;
-	// cpu->memory[0x0019] = 0x00;
-	//
-	gb->cpu->memory[0x0020] = 0x10; /* STOP */
-	// write(cpu, TAC, 0b101);
-
-	while (1) {
-		print_cpu_state(gb->cpu);
-		execute(gb->cpu);
-
-		if (gb->cpu->stop == 1)
-			break;
+	if (load_rom(gb->mem, "tetris.gb")) {
+		return 1;
 	}
-		print_cpu_state(gb->cpu);
 
-	getchar();
+	gb_run(gb);
 	return 0;
 }
